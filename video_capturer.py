ed7f777cd0739a518969fc3d56def2d864805630
Vladyslav <v.dusiak@gmail.com>
2021-06-08 01:00:34 +0300

    Added async sender and logging

--- video_capturer.py
+++ video_capturer.py
@@ -3,18 +3,20 @@
 import os
 import logging
 import argparse
-import picamera
-import picamera.array
+import threading
 
 import numpy as np
 
 from pathlib import Path
 from telegram.ext import Updater
 from datetime import datetime as dtm
+from picamera.array import PiMotionAnalysis
+from picamera import PiCamera, Color as PiColor
+from logging.handlers import RotatingFileHandler
 
 
 def get_args():
-    """Arguments parser."""
+    """Arguments parser and checker."""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--token', required=True,
                         help='Token for your telegram bot.')
@@ -34,13 +36,19 @@ def get_args():
                         help='Frame rotation.')
     parser.add_argument('--duration', type=int, default=5,
                         help='Duration of videos in seconds.')
+    parser.add_argument('--log-file',
+                        help='Path to log file for logging.')
     parser.add_argument('--save-to', type=Path,
                         help='Path to save dir.')
 
-    return parser.parse_args()
+    # check args
+    args = parser.parse_args()
+    assert args.duration > 2, 'duration is too low.'
+
+    return args
 
 
-class DetectMotion(picamera.array.PiMotionAnalysis):
+class DetectMotion(PiMotionAnalysis):
     def __init__(self, camera, vectors_quorum=10, magnitude_th=60):
         super().__init__(camera)
         self.magnitude_th = magnitude_th
@@ -57,62 +65,100 @@ class DetectMotion(picamera.array.PiMotionAnalysis):
             self.motion = True
 
 
+def send_record(bot, chat_id, h264_path, fps):
+    """Convert a record to from h264 to mp4 and send it to a telegram channel.
+
+    Parameters
+    ----------
+    bot : telegram.ext.Updater.bot
+        Telegram bot instance.
+    chat_id : str
+        Telegram chat id of your channel.
+    h264_path : pathlib.Path
+        Path to h264 record.
+    fps : int
+        Frames per seconds for h264 record.
+
+    """
+    # convert h264 to mp4
+    mp4_path = h264_path.parent.joinpath(h264_path.stem + '.mp4')
+    cmd = f'MP4Box -add {h264_path} -fps {fps} {mp4_path} >> /dev/null 2>&1'
+    os.system(cmd)
+
+    # send to telegram channel
+    bot.send_video(chat_id=chat_id,
+                   video=open(mp4_path, 'rb'),
+                   supports_streaming=True)
+
+    # clean up
+    os.remove(mp4_path)
+    os.remove(h264_path)
+
+
 def main():
     """Application entry point."""
     args = get_args()
-    logging.basicConfig(level=logging.INFO)
-    logging.info('Initializing.')
+
+    # setup logging
+    date_format = '%Y-%m-%d_%H-%M-%S'
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(levelname)s] %(asctime)s - %(message)s',
+                        datefmt=date_format)
+
+    logger = logging.getLogger('rpi_logger')
+    logger.info('Initializing')
+
+    if args.log_file is not None:
+        handler = RotatingFileHandler(args.log_file, 'w', 1024 * 1024 * 50, 5)
+        logger.addHandler(handler)
 
     # connect to telegram bot
     updater = Updater(token=args.token)
     updater.start_polling()
 
     # setup camera
-    camera = picamera.PiCamera(resolution=args.resolution, framerate=args.fps)
+    camera = PiCamera(resolution=args.resolution, framerate=args.fps)
     camera.rotation = args.rotation
-    camera.annotate_background = picamera.Color('black')
-    time_format = '%Y-%m-%d-%H-%M-%S'
-    camera.annotate_text = dtm.now().strftime(time_format)
+    camera.annotate_text_size = 14
+    camera.annotate_background = PiColor('black')
 
     # setup move detection
-    detection_data = {'640x480': (8, 20),
-                      '1280x720': (10, 40),
-                      '1920x1080': (12, 60)}
+    detection_data = {'640x480': (10, 20),
+                      '1280x720': (15, 40),
+                      '1920x1080': (20, 60)}
     vectors_quorum, magnitude_th = detection_data[args.resolution]
     output = DetectMotion(camera, vectors_quorum, magnitude_th)
 
-    logging.info('Initialization complete.')
-    logging.info('Start recording.')
+    logger.info('Initialization completed')
+    logger.info('Start recording')
     while True:
         start = dtm.now()
-        file_path = str(args.temp_dir.joinpath(start.strftime(time_format)))
+        h264_path = args.temp_dir.joinpath(
+            start.strftime(date_format) + '.h264')
 
         output.motion = False
-        camera.start_recording(file_path + '.h264', format='h264',
+        camera.annotate_text = dtm.now().strftime(date_format)
+        camera.start_recording(str(h264_path),
+                               format='h264',
                                motion_output=output)
 
         # make a record
         while (dtm.now() - start).seconds < args.duration:
-            camera.annotate_text = dtm.now().strftime(time_format)
+            camera.annotate_text = dtm.now().strftime(date_format)
             camera.wait_recording(0.2)
 
         # save file
         camera.stop_recording()
 
+        # if there was a motion - convert and send video asynchronously
         if output.motion:
-            # convert to mp4
-            os.system(f'MP4Box -add {file_path}.h264 '
-                      f'-fps {args.fps} {file_path}.mp4')
-
-            # send to telegram
-            updater.bot.send_video(
-                chat_id=args.chat_id,
-                video=open(f'{file_path}.mp4', 'rb'),
-                supports_streaming=True)
-
-            # clean up
-            os.remove(file_path + '.mp4')
-        os.remove(file_path + '.h264')
+            logger.warning('Motion detected, sending a record')
+            threading.Thread(
+                target=send_record,
+                args=[updater.bot, args.chat_id, h264_path, args.fps]
+            ).start()
+        else:
+            os.remove(h264_path)
 
 
 if __name__ == '__main__':
