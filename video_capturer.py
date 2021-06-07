9296d4299dfc04ecf8684677a03f6d37f381513b
Vladyslav <v.dusiak@gmail.com>
2021-06-07 23:56:41 +0300

    Added motion detection

--- video_capturer.py
+++ video_capturer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 import os
+import logging
 import argparse
 import picamera
+import picamera.array
+
+import numpy as np
 
 from pathlib import Path
 from telegram.ext import Updater
@@ -36,37 +40,66 @@ def get_args():
     return parser.parse_args()
 
 
+class DetectMotion(picamera.array.PiMotionAnalysis):
+    def __init__(self, camera, vectors_quorum=10, magnitude_th=60):
+        super().__init__(camera)
+        self.magnitude_th = magnitude_th
+        self.vectors_quorum = vectors_quorum
+        self.motion = False
+
+    def analyze(self, motions):
+        motions = np.sqrt(
+            np.square(motions['x'].astype(np.float)) +
+            np.square(motions['y'].astype(np.float))
+        ).clip(0, 255).astype(np.uint8)
+
+        if (motions > self.magnitude_th).sum() > self.vectors_quorum:
+            self.motion = True
+
+
 def main():
     """Application entry point."""
     args = get_args()
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Initializing.')
 
-    print('Initializing...')
+    # connect to telegram bot
     updater = Updater(token=args.token)
     updater.start_polling()
 
+    # setup camera
+    camera = picamera.PiCamera(resolution=args.resolution, framerate=args.fps)
+    camera.rotation = args.rotation
+    camera.annotate_background = picamera.Color('black')
     time_format = '%Y-%m-%d-%H-%M-%S'
-    with picamera.PiCamera(resolution=args.resolution,
-                           framerate=args.fps) as camera:
-        camera.rotation = args.rotation
-        camera.start_preview()
-        camera.annotate_background = picamera.Color('black')
-        camera.annotate_text = dtm.now().strftime(time_format)
+    camera.annotate_text = dtm.now().strftime(time_format)
 
-        for _ in range(5):
-            print('Processing')
-            start = dtm.now()
-            file_path = str(args.temp_dir.joinpath(
-                start.strftime(time_format)))
-            camera.start_recording(file_path + '.h264')
+    # setup move detection
+    detection_data = {'640x480': (8, 20),
+                      '1280x720': (10, 40),
+                      '1920x1080': (12, 60)}
+    vectors_quorum, magnitude_th = detection_data[args.resolution]
+    output = DetectMotion(camera, vectors_quorum, magnitude_th)
 
-            # record 10 seconds
-            while (dtm.now() - start).seconds < args.duration:
-                camera.annotate_text = dtm.now().strftime(time_format)
-                camera.wait_recording(0.2)
+    logging.info('Initialization complete.')
+    logging.info('Start recording.')
+    while True:
+        start = dtm.now()
+        file_path = str(args.temp_dir.joinpath(start.strftime(time_format)))
 
-            # save file
-            camera.stop_recording()
+        output.motion = False
+        camera.start_recording(file_path + '.h264', format='h264',
+                               motion_output=output)
 
+        # make a record
+        while (dtm.now() - start).seconds < args.duration:
+            camera.annotate_text = dtm.now().strftime(time_format)
+            camera.wait_recording(0.2)
+
+        # save file
+        camera.stop_recording()
+
+        if output.motion:
             # convert to mp4
             os.system(f'MP4Box -add {file_path}.h264 '
                       f'-fps {args.fps} {file_path}.mp4')
@@ -79,6 +112,7 @@ def main():
 
             # clean up
             os.remove(file_path + '.mp4')
+        os.remove(file_path + '.h264')
 
 
 if __name__ == '__main__':
